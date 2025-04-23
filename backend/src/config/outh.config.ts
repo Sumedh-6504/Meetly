798a9016da74bd52441404c4d035630ade9ada76
Sumedh-6504 <152703806+Sumedh-6504@users.noreply.github.com>
2025-04-23 22:54:57 +0530

    Update outh.config.ts

--- backend/src/config/outh.config.ts
+++ backend/src/config/outh.config.ts
@@ -1,11 +1,16 @@
 import { google } from "googleapis";
 import { config } from "./app.config";
 
+const redirectUri =
+  config.NODE_ENV === "production"
+    ? config.GOOGLE_REDIRECT_URI
+    : "http://localhost:8000/api/integration/google/calls";
+
 // Google Auth
 export const googleOAuth2Client = new google.auth.OAuth2(
   config.GOOGLE_CLIENT_ID,
   config.GOOGLE_CLIENT_SECRET,
-  config.GOOGLE_REDIRECT_URI
+  redirectUri
 );
 
 // Zoom Auth
